30229505586eeea5ea85864bcf193037a9670866
ankit-pandit07 <panditankit865@gmail.com>
2025-09-24 15:48:51 +0530

    init

---
+++ backend/middlewares/authUser.js
@@ -0,0 +1,18 @@
+
+import jwt from "jsonwebtoken";
+
+export const authUser =  (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized", success: false });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.id;
+    next();
+  } catch (error) {
+    console.error("Authentication error:", error);
+    return res.status(401).json({ message: "Invalid token", success: false });
+  }
+};
+
